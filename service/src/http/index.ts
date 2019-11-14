08f8c66c5c0b2b016d443ccd8b055df65654d0d5
PH <1836071395@qq.com>
2019-11-15 07:54:13 +0800

    add cache

--- service/src/http/index.ts
+++ service/src/http/index.ts
@@ -1,17 +1,23 @@
-import Koa from 'koa'
+import Koa, { ParameterizedContext } from 'koa'
 import koaStatic from 'koa-static'
 import path from 'path'
 import koaSslify from 'koa-sslify'
 import https from 'https'
 import http from 'http'
-import fs from 'fs'
+import fs, { Stats } from 'fs'
 import { sslKeyPath, sslCrtPath } from '../config/paths'
 
 const app = new Koa()
 const pwd = process.env.PWD||''
 const staticPath = path.resolve(pwd,'static')
 app.use(koaSslify())
-app.use(koaStatic(staticPath))
+app.use(koaStatic(staticPath, {
+  setHeaders: (res: ParameterizedContext["res"], path: string, stats: Stats) =>{
+    if(!/\/static\/(index|manifest)/.test(path)){
+      res.setHeader('Cache-Control','max-age=315360000')
+    }
+  }
+}))
 
 const options = {
   key: fs.readFileSync(sslKeyPath),
